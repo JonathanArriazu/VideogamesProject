aed799da59fc69e744f4e40912ef28e29eef4e23
JoniArriazu <joni.arriazu2@gmail.com>
2022-04-17 21:49:19 -0300

    Adding new files and styles

---
+++ src/js/iniciar-sesion.js
@@ -0,0 +1,38 @@
+//variables
+const usuarioLogin = document.querySelector(".grupo_usuarioLogin input");
+const passwordLogin = document.querySelector(".grupo_passwordLogin input");
+const formLogin = document.getElementById("formLogin");
+
+formLogin.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const usuario = usuarioLogin.value;
+  const password = passwordLogin.value;
+  if (usuario && password) {
+    login(usuario, password);
+  } else {
+    console.log("No entra");
+  }
+});
+
+const login = async (usuario, password) => {
+  try {
+    const response = await fetch(
+      `http://localhost:4000/usuario?usuario=${usuario}`
+    );
+    const usuarios = await response.json();
+
+    if (usuarios.length && usuarios[0].password === password) {
+      if (usuarios[0].admin) {
+        console.log("Es admin");
+        window.location.href = "./pruebadb.html";
+      } else {
+        console.log("No es admin");
+        window.location.href = "./index.html";
+      }
+    } else {
+      alert("Usuario/Contraseña Incorrecto");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
---
+++ src/js/registrar-usuario.js
@@ -0,0 +1,56 @@
+const inputs = document.querySelectorAll(".form_register input");
+const btnFormRegister = document.querySelector(".form_register button");
+const myForm = document.getElementById("myForm");
+const nombre = document.querySelector(".grupo_nombre input");
+const usuario = document.querySelector(".grupo_usuario input");
+const correo = document.querySelector(".grupo_correo input");
+const password = document.querySelector(".grupo_password input");
+
+myForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (nombre && usuario && correo && password) {
+    createUser();
+  } else {
+    console.log("No entra");
+  }
+});
+
+const createUser = async () => {
+  try {
+    await fetch("http://localhost:4000/usuario", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        nombre: nombre.value,
+        usuario: usuario.value,
+        correo: correo.value,
+        password: password.value,
+        admin: false,
+      }),
+    });
+    form_register.reset();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+/* myForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const formData = new FormData(this); // --> this se refiere al form. FormData se refiere a una coleccion de key values que contiene a los valores de username, password, email, etc.
+
+  fetch("http://localhost:4000/usuario", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      formData,
+    }),
+  }).catch(function (error) {
+    console.error(error);
+  });
+});
+ */
